e06c31e7382a284e87ea94c742dc0b288df739ff
Rustam <samandarzodarustam@gmail.com>
2023-02-21 12:01:47 +0800

    go

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+import style from "./App.module.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [cnt, setCnt] = useState(0)
+  const inc = ()=>{setCnt(cnt+1)};
+  const dec = ()=>{setCnt(cnt-1)};
+  return( 
+  <div>
+      <button onClick={inc}>+</button>
+        <h1>{cnt}</h1>
+        <button onClick={dec}>-</button>
+      
+  </div>
+  )
+}
+
+export default App;
